58bd1b5e622703db6664969a0e6f5bf2fbf1ff85
dadikas <73420676+dadikas@users.noreply.github.com>
2024-07-07 07:39:52 +0700

    initial commit

---
+++ build/vendors/imgui_opengl/CMakeFiles/imgui_opengl.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgui_opengl.dir/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/imgui_opengl.dir/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/imgui_opengl.dir/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/imgui_opengl.dir/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/imgui_opengl.dir/imgui.cpp.o"
+  "CMakeFiles/imgui_opengl.dir/imgui.cpp.o.d"
+  "CMakeFiles/imgui_opengl.dir/imgui_demo.cpp.o"
+  "CMakeFiles/imgui_opengl.dir/imgui_demo.cpp.o.d"
+  "CMakeFiles/imgui_opengl.dir/imgui_draw.cpp.o"
+  "CMakeFiles/imgui_opengl.dir/imgui_draw.cpp.o.d"
+  "CMakeFiles/imgui_opengl.dir/imgui_tables.cpp.o"
+  "CMakeFiles/imgui_opengl.dir/imgui_tables.cpp.o.d"
+  "CMakeFiles/imgui_opengl.dir/imgui_widgets.cpp.o"
+  "CMakeFiles/imgui_opengl.dir/imgui_widgets.cpp.o.d"
+  "libimgui_opengl.a"
+  "libimgui_opengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgui_opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/vendors/imgui_opengl/CMakeFiles/imgui_opengl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for imgui_opengl.
+# This may be replaced when dependencies are built.
